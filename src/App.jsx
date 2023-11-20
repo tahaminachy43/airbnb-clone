171fdf4b49da9f4c88bed3652516b0699a017c2a
Code_with_Moon <tahaminachy43@gmail.com>
2023-11-20 00:24:38 -0700

    better cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 
-// import star from "./assets/star.png"
 import Nav from "./nav";
-// import Content from "./content"
 import Card from "./card";
-// import katie from "./assets/katie-zaferes.png"
 import data from "./data";
-// import star from "./assets/star.png"
-// "parser": "@babel/eslint-parser";
 
 export default function App() {
   const cards = data.map((item) => {
     
     return(<Card
       Key = {item.id}
-      img = {item.img}
-      rating = {item.stats.rating}
-      reviewCount = {item.stats.reviewCount}
-      location = {item.location}
-      title = {item.title}
-      price = {item.price}
-      openSpots = {item.openSpots}
-    />
-    
+      item = {item}
+    /> 
     );
-
-  }
-  
-    
-  )
+  })
 
   return (
     <div>
