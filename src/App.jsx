bf63e9d6dea70e21fdab6a1828e39f507760e6d6
Code_with_Moon <tahaminachy43@gmail.com>
2023-11-19 22:12:55 -0700

    fixed but not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import Nav from "./nav"
-import Content from "./content"
-import Card from "./card"
-import katie from "./assets/katie-zaferes.png"
+
+// import star from "./assets/star.png"
+import Nav from "./nav";
+// import Content from "./content"
+import Card from "./card";
+// import katie from "./assets/katie-zaferes.png"
 import data from "./data";
-import star from "./assets/star.png"
+// import star from "./assets/star.png"
+// "parser": "@babel/eslint-parser";
 
 export default function App() {
-  const cards = data.map(item => {
-    return(
-      <Card
+  const cards = data.map((item) => {
+    console.log(item);
+    return(<Card
+    Key = {item.id}
+    img = {item.coverImg}
+    rating = {item.stats.rating}
+    reviewers = {item.stats.reviewCount}
+    country = {item.location}
+    title = {item.location}
+    price = {item.price}
+    />
+    
+    );
 
-        img = {item.coverImg}
-        rating = {item.stats.rating}
-        reviewers = {item.stats.reviewCount}
-        country = {item.location}
-        title = {item.location}
-        price = {item.price}
-
-      />
-    )
-  })
+  }
+  
+    
+  )
 
   return (
     <div>
@@ -27,4 +34,5 @@ export default function App() {
       {cards}
 
     </div>
+  )
 }
--- src/card.jsx
+++ src/card.jsx
@@ -1,20 +1,20 @@
-/* eslint-disable react/prop-types */
 
+import star from "./assets/star.png"
 export default function Card(props){
     console.log(props)
     return(
         <div className="card">
             
-            <img src= {props.img} alt = "katie" className="katie"/>
+            <img src= {`../src/assets/${this.props.img}`} alt = "katie" className="katie"/>
             <div className="cardstack">
-                <img src= {props.img2} alt="star" className="star"/>
-                <span> {props.rating}</span>
-                <span className="grey">({props.reviewers}) . </span>
-                <span className="grey">{props.country} </span>
+                <img src= {star} alt="star" className="star"/>
+                <span> {this.props.rating}</span>
+                <span className="grey">({this.props.reviewCount}) . </span>
+                <span className="grey">{this.props.location} </span>
 
             </div>
-            <p> {props.title} </p>
-            <p> From ${props.price} / person</p>
+            <p> {this.props.title} </p>
+            <p> From ${this.props.price} / person</p>
 
         </div>
     )
